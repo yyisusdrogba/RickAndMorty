e364e670ba34b9b4883604c3798f96a895dc94ad
Jesus Garcia <jesus.gg.dev@gmail.com>
2025-07-07 16:21:42 -0600

    Initial Commit

---
+++ RickAndMortyProgress/RickAndMortyProgressApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyProgressApp.swift
+//  RickAndMortyProgress
+//
+//  Created by Jesus Gonzalez on 07/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyProgressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
